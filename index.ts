7b0df96ad6be653bab08f4d87978b0191d5412e0
fakeJan499 <dawid.sz@tutanota.com>
2021-01-08 18:21:51 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -5,18 +5,19 @@ class Game {
   static readonly boardWidth = 40;
   static readonly boardHeight = 20;
 
-  private speedInMs$: Subject<number> = new BehaviorSubject(500);
+  private speedInMs$: Subject<number> = new BehaviorSubject(550);
   private score$: Subject<number> = new BehaviorSubject(0);
   private counter = 0;
+  private state = [];
 
-  getRandomLetter(): string {
+  private getRandomLetter(): string {
     return String.fromCharCode(
       Math.random() * ("z".charCodeAt(0) - "a".charCodeAt(0)) +
         "a".charCodeAt(0)
     );
   }
 
-  printBoard(board: Letter[]): void {
+  private printBoard(board: Letter[]): void {
     let content = "";
 
     for (let i = 0; i < Game.boardHeight; i++) {
@@ -56,14 +57,15 @@ class Game {
                   this.score$
                     .pipe(take(1))
                     .subscribe(score => this.score$.next(score + 10));
-                  if (++this.counter === 25) {
+                  if (++this.counter === 20) {
+                    this.state = acc.slice(1);
                     this.counter = 0;
                     this.speedInMs$.next(ms - 25);
                   }
                   return acc.slice(1);
                 } else return acc;
               }
-            }, [])
+            }, this.state)
           )
         ),
         tap(val => this.printBoard(val)),
@@ -85,7 +87,13 @@ class Game {
 
   private gameOver() {
     document.body.innerHTML =
-      '<p>Game Over</p><button id="new_game_button">New Game</button>';
+      '<p>Game Over</p><p id="score"></p><button id="new_game_button">New Game</button>';
+    this.score$
+      .pipe(take(1))
+      .subscribe(
+        score =>
+          (document.getElementById("score").textContent = score.toString())
+      );
     document
       .getElementById("new_game_button")
       .addEventListener("click", () => this.startGame());
